0b1d9af9a18ac48afc13d786f6cba3b348d4db12
Meng <50622152+Anneli98@users.noreply.github.com>
2026-09-09 17:09:38 +0200

    Update load_subfunctions.R

--- R/subfunctions/load_subfunctions.R
+++ R/subfunctions/load_subfunctions.R
@@ -4,7 +4,7 @@
 
 source(here::here("R", "subfunctions", "base_functions.R"))
 source(here::here("R", "subfunctions", "community", "auxiliary_model.R"))
-source(here::here("R", "subfunctions", "community", "rjse_spectral_clustering.R"))
+source(here::here("R", "subfunctions", "community", "RJSE.R"))
 source(here::here("R", "subfunctions", "laem_core.R"))
 source(here::here("R", "subfunctions", "marginal_likelihood.R"))
 source(here::here("R", "subfunctions", "inference_core.R"))
